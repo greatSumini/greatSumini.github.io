df39573d8f654c5a480c5c66096f485179b31d78
greatSumini <greatsumini@gmail.com>
2020-09-06 22:49:46 +0900

    build(yarn): add gatsby-plugin-typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,22 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react';
+import { Link, graphql, PageProps } from 'gatsby';
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import Bio from '../components/bio';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { rhythm } from '../utils/typography';
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function HomePage({ data, location }: PageProps<any>) {
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMarkdownRemark.edges;
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
       {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
+        const title = node.frontmatter.title || node.fields.slug;
         return (
           <article key={node.fields.slug}>
             <header>
@@ -38,13 +39,11 @@ const BlogIndex = ({ data, location }) => {
               />
             </section>
           </article>
-        )
+        );
       })}
     </Layout>
-  )
+  );
 }
-
-export default BlogIndex
 
 export const pageQuery = graphql`
   query {
@@ -69,4 +68,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
