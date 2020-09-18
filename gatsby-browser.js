8a515d8ca0eab7aad46876775e35881c7da72338
greatSumini <greatsumini@gmail.com>
2020-09-19 04:37:19 +0900

    feat: :lipstick: add styles/syntax-highlight/xml.css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,10 @@ import 'prismjs/themes/prism-solarizedlight.css';
 
 import './src/styles/global.css';
 import './src/styles/post.css';
+
+// syntax-highlights
 import './src/styles/syntax-highlight/tsx.css';
 import './src/styles/syntax-highlight/typescript.css';
 import './src/styles/syntax-highlight/json.css';
 import './src/styles/syntax-highlight/shell.css';
+import './src/styles/syntax-highlight/xml.css';
