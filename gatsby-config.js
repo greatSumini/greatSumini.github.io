49a9c5498460e50df1ccb22fbe3c5f7e95c3efb0
greatSumini <greatsumini@gmail.com>
2020-09-20 19:46:17 +0900

    build(yarn): :package: install gatsby-remark-relative-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,18 +31,13 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-relative-images`,
           {
             resolve: `gatsby-remark-images`,
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
               maxWidth: 800,
-              linkImagesToOriginal: true,
-              wrapperStyle: (fluidResult) =>
-                `flex:${Math.round(fluidResult.aspectRatio, 2)}; max-width: ${
-                  Math.round(fluidResult.aspectRatio, 2) * 300
-                }px;`,
+              linkImagesToOriginal: false,
+              sizeByPixelDensity: true,
             },
           },
           {
