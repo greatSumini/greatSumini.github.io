dfdb909074a7ee22fa8528a768502fc3981b3e25
greatSumini <greatSumini@users.noreply.github.com>
2021-08-20 20:43:26 +0000

    Deploy to GitHub Pages

---
+++ component---src-pages-404-tsx-475dc0a86522b34ffa39.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[218],{938:function(t,e,a){"use strict";a.r(e);var n=a(7294),l=a(9726),s=a(6590);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-475dc0a86522b34ffa39.js.map
